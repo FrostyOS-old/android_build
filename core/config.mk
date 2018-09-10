e4307cd5de3f34cd3bb3e0e04909ab7bcdb2890e
Sebastian (Felvesthe) <xdzienny@gmail.com>
2018-09-10 17:01:06 +0200

    build: Disable our sepolicy for now
    Signed-off-by: Sebastian (Felvesthe) <xdzienny@gmail.com>

--- core/config.mk
+++ core/config.mk
@@ -1097,7 +1097,7 @@ endif
 ifneq ($(FROSTY_BUILD),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
-$(eval include device/frosty/sepolicy/common/sepolicy.mk)
+#$(eval include device/frosty/sepolicy/common/sepolicy.mk)
 endif
 
 # Include any vendor specific config.mk file
